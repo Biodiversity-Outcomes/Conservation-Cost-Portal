d47c2c239a874627e931ecc86e77f177b49cdfd4
tfisher44 <taylorfisher9045@gmail.com>
2025-10-03 11:45:43 -0700

    Added content and styling to the Collecting, Studies, and Contact pages

--- src/pages/Reporting.jsx
+++ src/pages/Reporting.jsx
@@ -1,4 +1,5 @@
 import HeroSection from "../components/HeroSection.jsx"
+import "./Reporting.css"
 
 function Reporting(){
     return (
@@ -9,7 +10,33 @@ function Reporting(){
             />
 
             <div className="reporting-content">
-                <p>content</p>
+                    <p>
+                    Reporting on cost data that have been used in analyses or reports needs to be thorough enough so that they can interpreted appropriately. These materials walk through standards for what information should be included when data are reported and provide a worksheet for providing "metadata" for cost data when it is included in published papers or reports.
+                    ​</p>
+                    <ul>
+                        <li>
+                            <a href="https://conbio.onlinelibrary.wiley.com/doi/abs/10.1111/cobi.13195"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                Iacona et al 2018
+                            </a>
+                            : Cost Reporting Workbook for download under article's "Supporting Information"
+                        </li>
+                        <li>
+                            <a href="https://conbio.onlinelibrary.wiley.com/doi/epdf/10.1111/csp2.70007"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                ​Guerrero-Pineda et al 2025
+                            </a>
+                                : The article discusses the development of a data management system designed to enhance cost-efficiency in conservation projects by integrating cost data with intervention outcomes. It aids managers in making informed decisions on budget allocation by providing a structured approach to tracking and analyzing costs relative to conservation benefits, illustrated through a case study involving the red spiny lobster fishery in the Galápagos Marine Reserve.
+                            <br></br>
+                            <a href="https://306499c8-58fc-48ca-99b5-11d4d7e31a7b.filesusr.com/ugd/fa9b8c_6e8cf98e819640d09df6bf3822977603.docx?dn=csp270007-sup-0001-supinfo%20(2).docx"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                Supporting Information
+                            </a>
+                        </li>
+                    </ul>
             </div>
         </>
     )
--- src/pages/Studies_Data.jsx
+++ src/pages/Studies_Data.jsx
@@ -1,4 +1,5 @@
 import HeroSection from "../components/HeroSection.jsx"
+import "./Studies_Data.css"
 
 function Studies_Data(){
     return (
@@ -9,7 +10,72 @@ function Studies_Data(){
             />
 
             <div className="studies_data-content">
-                <p>content</p>
+                <p>
+                    We are compiling published literature (and eventually aim to include grey literature) that includes conservation intervention cost data.  
+                    The goal of this compilation is to provide examples of how costs have been used and reported, and also to track the extent of the literature.
+                </p>
+                <p>
+                    This viewable {" "}
+                    <a href="https://docs.google.com/spreadsheets/d/1oEjnBPJamU0iGZ5TUoRiiRNQCbbbAvmp8PHJWi7Xyb8/edit?usp=sharing"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        google document
+                    </a>
+                    {" "} details research studies we have come across that contain cost information.  
+                </p>
+                <p>
+                    If you would like to add a case study please use this {" "}
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfschXiXH9hAdfZFtoiHFb6h1nSiDTN9lAQmf4MHHIBG5_Ikw/viewform?usp=sf_link"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        google form
+                    </a>
+                    {" "} or email {" "}
+                    <a href="Gwen.Iacona@asu.edu"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        Dr. Gwen Iacona
+                    </a>
+                    {" "} with copies of papers or reports that would be useful to the community.
+                </p>
+                <h2>Cost Datasets</h2>
+                <p>
+                Cost datasets relevant to conservation efforts:
+                </p>
+                <ul>
+                    <li>
+                        The {" "}
+                        <a href="https://tescost.el.erdc.dren.mil/Default.aspx"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Army Corps of Engineering
+                        </a>
+                        {" "} has been tracking expenditures on {" "}
+                        <a href="https://tescost.el.erdc.dren.mil/Default.aspx"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            ESA listed species
+                        </a>
+                        {" "} compliance and recovery actions.
+                    </li>
+                    <li>
+                        <a href="https://tescost.el.erdc.dren.mil/Default.aspx"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Frank Courchamp and team
+                        </a>
+                        {" "} have been developing a database on the costs of invasion.
+                    </li>
+                    <li>
+                        <a href="https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/1365-2664.14377"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Yong et al 2023
+                        </a>
+                        {" "} — Costs for threat abatement strategies across Australia show significant variation, ranging from $24 to $879,985 per square kilometer ($0.24–$8880 per hectare). 
+                        The costs encompass various components, including labor, consumables, travel, and equipment.
+                    </li>
+                </ul>
             </div>
         </>
     )
